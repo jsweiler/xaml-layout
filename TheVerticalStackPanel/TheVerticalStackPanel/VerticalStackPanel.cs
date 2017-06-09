59b29ab2ecc87faa842a0912c5424d6ed8226198
Jeff Weiler <jeffweiler8770@gmail.com>
2017-06-09 09:46:56 -0400

    Add the vertical stackpanel

---
+++ TheVerticalStackPanel/TheVerticalStackPanel/VerticalStackPanel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TheVerticalStackPanel
+{
+    public class VerticalStackPanel : Panel
+    {
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            var mySize = new Size();
+
+            foreach (UIElement child in this.InternalChildren)
+            {
+                child.Measure(availableSize);
+                mySize.Width += child.DesiredSize.Width;
+                mySize.Width += child.DesiredSize.Height;
+            }
+
+            return mySize;
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            var location = new Point();
+            foreach (UIElement child in this.InternalChildren)
+            {
+                child.Arrange(new Rect(location, child.DesiredSize));
+                location.X += child.DesiredSize.Width;
+                location.Y += child.DesiredSize.Height;
+            }
+
+            return finalSize;
+        }
+    }
+}
